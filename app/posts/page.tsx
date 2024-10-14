b993b865c609b3e81bfde93ab8aa181090bfab21
Brilian-Maker <brilianadam09@gmail.com>
2024-10-14 20:37:03 +0700

    menambahkan public

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,32 +1,32 @@
-import React from "react";
+import warframeData from "../public/data/war.json";
 import CardList from "../components/posts/CardList";
 import ViewUserButton from "../components/posts/ViewUserButton";
+import postcss from "postcss";
 
-const base_url = "https://jsonplaceholder.typicode.com/posts";
-
-interface Nxtpost {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+interface War {
+  IdFrame: number;
+  Nama: string;
+  Weapon: string;
+  Melee: string;
+  Sentinel: string;
 }
 
-const Posts = async () => {
-  const response = await fetch(base_url, {
-    next: { revalidate: 3600 }
-  });
-  const posts: Nxtpost[] = await response.json();
+const Posts = () => {
+  const posts: War[] = warframeData.warframes;
+
   return (
     <>
     <p>{new Date().toLocaleTimeString()}</p>
       <h1 className="text-fuchsia-500">POSTINGAN PAGE</h1>
       {posts.map((post) => {
         return (
-          <CardList key={post.id}>
-            <p>{post.id}</p>
-            <i>{post.title}</i>
-            <p>{post.body}</p>
-            <ViewUserButton userId={post.userId} />
+          <CardList key={post.IdFrame}>
+            <p>{post.IdFrame}</p>
+            <i>{post.Nama}</i>
+            <p>{post.Weapon}</p>
+            <p>{post.Melee}</p>
+            <p>{post.Sentinel}</p>
+            <ViewUserButton userId={post.IdFrame} />
           </CardList>
         );
       })}
